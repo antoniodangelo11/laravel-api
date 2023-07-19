ec3cb66123d9100cae2389701f915327173c962d
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-19 23:58:06 +0200

    set route and controller api for Type

---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Type;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TypeController extends Controller
+{
+    public function index()
+    {
+        $type = Type::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $type,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ use App\Http\Controllers\Api\ProjectController;
 
 Route::get('projects', [ProjectController::class, 'index'])->name('api.projects.index');
 Route::get('projects/{project}', [ProjectController::class, 'show'])->name('api.projects.show');
+Route::get('types', [TypeController::class, 'index'])->name('api.types.show');
 Route::post('leads/', [LeadController::class, 'store'])->name('api.leads.store');
