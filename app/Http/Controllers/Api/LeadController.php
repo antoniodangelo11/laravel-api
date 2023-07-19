4e8b35a51c7092c1244bdc943cabdf18362c91a0
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-19 23:48:36 +0200

    add guzzle model, controller and api lead

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+
+class LeadController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ use App\Http\Controllers\Api\ProjectController;
 
 Route::get('projects', [ProjectController::class, 'index'])->name('api.projects.index');
 Route::get('projects/{project}', [ProjectController::class, 'show'])->name('api.projects.show');
+Route::post('leads/', [LeadController::class, 'store'])->name('api.leads.store');
